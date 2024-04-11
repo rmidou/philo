5c00823be94ce2f9e8b889231ed3db7be51bf0d3
rmidou <139585550+rmidou@users.noreply.github.com>
2024-04-11 14:03:37 +0200

    Add files via upload

---
+++ philo/src/init.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmidou <rmidou@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 15:04:24 by rmidou            #+#    #+#             */
+/*   Updated: 2024/04/11 12:02:30 by rmidou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+int	args(char **av, t_main *philo)
+{
+	if (ft_isdigit(av[1]) && ft_isdigit(av[2]) && ft_isdigit(av[3])
+		&& ft_isdigit(av[4]))
+	{
+		if (ft_atoi(av[1]) == 1)
+			init_philo1(ft_atoi(av[2]));
+		else
+		{
+			philo->nb_philo = ft_atoi(av[1]);
+			philo->ttd = ft_atoi(av[2]);
+			philo->tte = ft_atoi(av[3]);
+			philo->tts = ft_atoi(av[4]);
+			philo->nbr_philo_eat = 2147483647;
+		}
+	}
+	else
+	{
+		printf("philosophers: wrong arguments: <number_of_philosophers> "
+			"<time_to_die> <time_to_eat> <time_to_sleep>"
+			"[number_of_times_each_philosopher_must_eat]\n");
+		return (0);
+	}
+	if (ft_isdigit(av[5]))
+		philo->nbr_philo_eat = ft_atoi(av[5]);
+	return (1);
+}
+
+void	init(t_main *main)
+{
+	int	i;
+
+	i = 0;
+	main->philos = malloc(sizeof(t_philo) * main->nb_philo);
+	main->forks = malloc(sizeof(pthread_mutex_t) * main->nb_philo);
+	while (i < main->nb_philo)
+	{
+		ft_bzero(&main->philos[i], sizeof(t_philo));
+		main->philos[i].main = main;
+		main->philos[i].fork1 = &(main->forks[i]);
+		main->philos[i].fork2 = &(main->forks[(i + 1) % main->nb_philo]);
+		main->philos[i].ttd = main->ttd;
+		main->philos[i].tte = main->tte;
+		main->philos[i].tts = main->tts;
+		main->philos[i].id = i + 1;
+		i++;
+	}
+}
+
+void	create_mutex(t_main *main)
+{
+	int	i;
+
+	i = 0;
+	while (i < main->nb_philo)
+	{
+		pthread_mutex_init(&(main->forks[i]), NULL);
+		i++;
+	}
+}
+
+void	init_philo(t_main *main)
+{
+	int	i;
+
+	i = -1;
+	create_mutex(main);
+	while (++i < main->nb_philo)
+	{
+		pthread_create(&(main->philos[i].thread),
+			NULL, &routine, &main->philos[i]);
+	}
+	check_death(main);
+	i = -1;
+	while (++i < main->nb_philo)
+		pthread_join(main->philos[i].thread, NULL);
+}
+
+void	init_philo1(int ttd)
+{
+	printf("%d %d %s\n", 0, 1, "has taken a fork");
+	ft_usleep(ttd);
+	printf("%d %d %s\n", ttd, 1, "died");
+	exit(0);
+}
---
+++ philo/src/main.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmidou <rmidou@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 15:35:10 by rmidou            #+#    #+#             */
+/*   Updated: 2024/04/11 12:02:52 by rmidou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+void	clear_main(t_main *main)
+{
+	if (main->forks)
+		free(main->forks);
+	if (main->philos)
+		free(main->philos);
+}
+
+void	ft_exit(t_main *main)
+{
+	int	i;
+
+	i = -1;
+	while (++i < main->nb_philo)
+		pthread_mutex_destroy(&main->forks[i]);
+	clear_main(main);
+}
+
+uint64_t	get_time(void)
+{
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL))
+		return (0);
+	return ((tv.tv_sec * (uint64_t)1000) + (tv.tv_usec / 1000));
+}
+
+uint64_t	get_time2(t_main *main)
+{
+	return (get_time() - main->time_of_start);
+}
+
+int	main(int ac, char **av)
+{
+	t_main		main;
+
+	if (ac < 5)
+	{
+		printf("philosophers: wrong arguments: <number_of_philosophers>"
+			" <time_to_die> <time_to_eat> <time_to_sleep>"
+			"[number_of_times_each_philosopher_must_eat]");
+	}
+	ft_bzero(&main, sizeof(t_main));
+	if (ac < 4)
+		return (0);
+	if (args(av, &main) == 0)
+		return (0);
+	main.time_of_start = get_time();
+	init(&main);
+	init_philo(&main);
+	ft_exit(&main);
+	return (0);
+}
